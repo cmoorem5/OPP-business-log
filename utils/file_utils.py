50293343096423cb8b9cf5829839a8c1962431f3
cmoorem5 <cmoorem5@icloud.com>
2025-04-06 09:09:26 -0400

    Delete utils directory

--- utils/file_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def save_uploaded_file(uploaded_file, folder="receipts"):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    file_path = os.path.join(folder, uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return file_path
