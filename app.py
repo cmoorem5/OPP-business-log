1d73844047041033c4e41db2759b988bd0308320
cmoorem5 <cmoorem5@icloud.com>
2025-04-05 21:34:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pages import dashboard, log_entry, view_entries, receipts, export
 
-st.set_page_config(page_title="OPP Finance App", layout="wide")
+st.set_page_config(page_title="OPP Finance Tracker", layout="wide")
 
 st.sidebar.title("📘 OPP Finance Tracker")
 page = st.sidebar.radio("Navigate", ["Dashboard", "Log Entry", "View Entries", "Receipts", "Data Export"])
