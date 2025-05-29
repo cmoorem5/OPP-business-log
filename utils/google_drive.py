7289c2f38594972f3ab1d84a854c6868a8d7ff16
cmoorem5 <cmoorem5@icloud.com>
2025-05-29 17:25:37 -0400

    Update google_drive.py

--- utils/google_drive.py
+++ utils/google_drive.py
@@ -1,16 +1,18 @@
+import json
+import streamlit as st
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import service_account
-import streamlit as st
 
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 def _get_drive_service():
-    creds_dict = st.secrets["gdrive_credentials"]  # Already a dict
+    creds_raw = st.secrets["gdrive_credentials"]
+    creds_dict = json.loads(creds_raw) if isinstance(creds_raw, str) else creds_raw
     creds = service_account.Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
     return build("drive", "v3", credentials=creds)
 
-def upload_file_to_drive(uploaded_file, folder_id, filename):
+def upload_file_to_drive(uploaded_file, filename: str, folder_id: str) -> str:
     service = _get_drive_service()
     media = MediaIoBaseUpload(uploaded_file, mimetype=uploaded_file.type)
     file_metadata = {
@@ -24,5 +26,5 @@ def upload_file_to_drive(uploaded_file, folder_id, filename):
     ).execute()
     return file.get("id")
 
-def generate_drive_link(file_id):
+def generate_drive_link(file_id: str) -> str:
     return f"https://drive.google.com/file/d/{file_id}/view"
