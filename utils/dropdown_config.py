632f2b9dbca8f7031538c41372c0e81de823208e
cmoorem5 <cmoorem5@icloud.com>
2025-06-02 13:11:55 -0400

    Create dropdown_config.py

---
+++ utils/dropdown_config.py
@@ -0,0 +1,21 @@
+# utils/dropdown_config.py
+
+INCOME_YEARS = ["2025", "2026"]
+
+PROPERTIES = ["Isamorada", "Standish"]
+
+INCOME_SOURCES = ["Zelle", "Check", "Cash", "Venmo", "Other"]
+
+EXPENSE_CATEGORIES = [
+    "Property Expense", "Furnishings & Supplies", "Guest & Operational Expenses",
+    "Travel & Transportation", "Legal & Professional Services", "Food & Beverage",
+    "Taxes & Compliance", "Business Expansion & Improvements", "Misc & Other"
+]
+
+PURCHASERS = [
+    "Cash", "Debit C6270", "JB C6443B", "JB J5062B", "JB C1112",
+    "JB J0186", "JB J7698", "Jordans", "OPP Checking",
+    "OPP JetBlue", "Return", "TJ MAXX Card"
+]
+
+PAYMENT_STATUS = ["Paid", "PMT due", "Downpayment received"]
